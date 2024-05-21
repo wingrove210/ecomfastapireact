1b7d4ac8497ead291cea4ec386dc85174ecb6207
wingrove210 <126200528+wingrove210@users.noreply.github.com>
2024-05-21 21:04:14 +0300

    fastapi
    fastapi

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -7,12 +7,12 @@ load_dotenv()
 class Settings:
     PROJECT_NAME: str = "Simple Twitter"
     PROJECT_VERSION: str = "1.0.0"
-    USE_SQLITE_DB: str = os.getenv("USE_SQLITE_DB")
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    USE_SQLITE_DB: str = os.getenv("USE_SQLITE_DB", False)
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "baracella38")
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "postgres")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
